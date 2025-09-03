61280fc087a5739f70d6149155a37bbd2fc46520
running-life <baorun.chen01@gmail.com>
2025-09-03 17:05:09 +0800

    triangle

---
+++ sanbox/triangle/main.cpp
@@ -0,0 +1,116 @@
+#include <donut/app/ApplicationBase.h>
+#include <donut/app/DeviceManager.h>
+#include <donut/core/log.h>
+#include <donut/core/vfs/VFS.h>
+#include <donut/engine/ShaderFactory.h>
+#include <nvrhi/utils.h>
+
+#define _STRINGIFY(s) #s
+#define STRINGIFY(s)  _STRINGIFY(s)
+
+using namespace donut;
+
+static const char* g_WindowTitle = "Donut Example: Basic Triangle";
+
+class BasicTriangle : public app::IRenderPass {
+private:
+    nvrhi::ShaderHandle m_VertexShader;
+    nvrhi::ShaderHandle m_PixelShader;
+    nvrhi::GraphicsPipelineHandle m_Pipeline;
+    nvrhi::CommandListHandle m_CommandList;
+
+public:
+    using IRenderPass::IRenderPass;
+
+    bool Init() {
+        std::filesystem::path appShaderPath = app::GetDirectoryWithExecutable() / "shaders" / STRINGIFY(PROJECT_NAME) / app::GetShaderTypeName(GetDevice()->getGraphicsAPI());
+
+        auto nativeFS = std::make_shared<vfs::NativeFileSystem>();
+        engine::ShaderFactory shaderFactory(GetDevice(), nativeFS, appShaderPath);
+
+        m_VertexShader = shaderFactory.CreateShader("shaders.hlsl", "main_vs", nullptr, nvrhi::ShaderType::Vertex);
+        m_PixelShader = shaderFactory.CreateShader("shaders.hlsl", "main_ps", nullptr, nvrhi::ShaderType::Pixel);
+
+        if (!m_VertexShader || !m_PixelShader) {
+            return false;
+        }
+
+        m_CommandList = GetDevice()->createCommandList();
+
+        return true;
+    }
+
+    void BackBufferResizing() override {
+        m_Pipeline = nullptr;
+    }
+
+    void Animate(float fElapsedTimeSeconds) override {
+        GetDeviceManager()->SetInformativeWindowTitle(g_WindowTitle);
+    }
+
+    void Render(nvrhi::IFramebuffer* framebuffer) override {
+        if (!m_Pipeline) {
+            nvrhi::GraphicsPipelineDesc psoDesc;
+            psoDesc.VS = m_VertexShader;
+            psoDesc.PS = m_PixelShader;
+            psoDesc.primType = nvrhi::PrimitiveType::TriangleList;
+            psoDesc.renderState.depthStencilState.depthTestEnable = false;
+
+            m_Pipeline = GetDevice()->createGraphicsPipeline(psoDesc, framebuffer);
+        }
+
+        m_CommandList->open();
+
+        nvrhi::utils::ClearColorAttachment(m_CommandList, framebuffer, 0, nvrhi::Color(0.f));
+
+        nvrhi::GraphicsState state;
+        state.pipeline = m_Pipeline;
+        state.framebuffer = framebuffer;
+        state.viewport.addViewportAndScissorRect(framebuffer->getFramebufferInfo().getViewport());
+
+        m_CommandList->setGraphicsState(state);
+
+        nvrhi::DrawArguments args;
+        args.vertexCount = 3;
+        m_CommandList->draw(args);
+
+        m_CommandList->close();
+        GetDevice()->executeCommandList(m_CommandList);
+    }
+};
+
+#ifdef WIN32
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#else
+int main(int __argc, const char** __argv)
+#endif
+{
+    nvrhi::GraphicsAPI api = app::GetGraphicsAPIFromCommandLine(__argc, __argv);
+    app::DeviceManager* deviceManager = app::DeviceManager::Create(api);
+
+    app::DeviceCreationParameters deviceParams;
+#ifdef _DEBUG
+    deviceParams.enableDebugRuntime = true;
+    deviceParams.enableNvrhiValidationLayer = true;
+#endif
+
+    if (!deviceManager->CreateWindowDeviceAndSwapChain(deviceParams, g_WindowTitle)) {
+        log::fatal("Cannot initialize a graphics device with the requested parameters");
+        return 1;
+    }
+
+    {
+        BasicTriangle example(deviceManager);
+        if (example.Init()) {
+            deviceManager->AddRenderPassToBack(&example);
+            deviceManager->RunMessageLoop();
+            deviceManager->RemoveRenderPass(&example);
+        }
+    }
+
+    deviceManager->Shutdown();
+
+    delete deviceManager;
+
+    return 0;
+}
